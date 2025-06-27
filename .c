765cb0b7319b8050a65a9147f9bfbf475903be2d
s1320253 <s1320253@u-aizu.ac.jp>
2025-06-27 12:18:43 +0900

    Dice game: initial revision

--- .c
+++ .c
@@ -1,21 +1,20 @@
-    #include<stdio.h>
-    #include<stdlib.h>
-    #include<time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
     
-    int main(){
-      char name[100];
-    int dice1,dice2,total;
-    srand(time(NULL));
+int main(){
+int dice1,dice2,total;
+srand(time(NULL));
     
-    printf("Rolling dice...\n");
+printf("Rolling dice...\n");
     
-    dice1 = rand() % 6+1;
-    dice2 = rand() % 6+1;
-    total = dice1+dice2;
+dice1 = rand() % 6+1;
+dice2 = rand() % 6+1;
+total = dice1+dice2;
     
-    printf("Die 1: %d\n",dice1);
-    printf("Die 2; %d\n",dice2);
-    printf("total value: %d\n",total);
+printf("Die 1: %d\n",dice1);
+printf("Die 2; %d\n",dice2);
+printf("total value: %d\n",total);
     
-    return 0;
-    }
+return 0;
+}
