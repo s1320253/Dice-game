09270552208d3d209b3d02ee763c0dd9e5ef3749
s1320253 <s1320253@u-aizu.ac.jp>
2025-06-27 12:17:17 +0900

    Greetings added

--- .c
+++ .c
@@ -1,21 +1,24 @@
-    #include<stdio.h>
-    #include<stdlib.h>
-    #include<time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
     
-    int main(){
-      char name[100];
-    int dice1,dice2,total;
-    srand(time(NULL));
+int main(){
+char name[100];
+int dice1,dice2,total;
+srand(time(NULL));
+printf("What is your name?\n");
+scanf("%s",name);
+printf("Hello,%s\n",name);
+　　
+printf("Rolling dice...\n");
     
-    printf("Rolling dice...\n");
+dice1 = rand() % 6+1;
+dice2 = rand() % 6+1;
+total = dice1+dice2;
     
-    dice1 = rand() % 6+1;
-    dice2 = rand() % 6+1;
-    total = dice1+dice2;
+printf("Die 1: %d\n",dice1);
+printf("Die 2; %d\n",dice2);
+printf("total value: %d\n",total);
     
-    printf("Die 1: %d\n",dice1);
-    printf("Die 2; %d\n",dice2);
-    printf("total value: %d\n",total);
-    
-    return 0;
-    }
+return 0;
+}
